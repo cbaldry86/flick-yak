261496360d9e729d8228305b194b48a0db2659c6
Craig baldry <cbaldry@our.ecu.edu.au>
2021-08-03 22:57:44 +0800

    add member registration

---
+++ views/public/member_register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <script src="../../scripts/validate_registration.js"></script>
+</head>
+
+<body>
+    <?php require_once '../common/nav.php' ?>
+    <h2>Member Registration</h2>
+    <a href="../../index.php">Back Home</a>
+    <?php require_once '../forms/register_form.php'?>
+</body>
+
+</html>

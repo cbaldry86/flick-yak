cbbadc7fb456ce43483b13479680ea7867ed2aab
Craig baldry <cbaldry@our.ecu.edu.au>
2021-08-10 13:41:04 +0800

    fix up user profile page

--- views/member/user_profile.php
+++ views/member/user_profile.php
@@ -5,9 +5,9 @@ if (!isset($_SESSION['access_level'])) {
     require '../errors/401.php';
     exit;
 }
-require 'db_connect.php';
+require '../../src/connect_db.php';
 
-if (!isset($_GET['id']) || !ctype_digit($_GET['id'])) {
+if (!isset($_GET['id'])) {
     echo 'Invalid User ID.';
     exit;
 }
@@ -36,25 +36,16 @@ if (!$user) { // If no data (no thread with that ID in the database)
 
 <body>
     <?php
-
     require_once '../common/nav.php';
     echo '<h2>Details of "'.$user['username'].'"</h2>';
-
-    // Loop through each forum to generate an option of the drop-down list
-
-    echo '<p><a href="../member/user_profile.php?id=' . $row['username'] . '">'
-        . $row['username'] . '</a>';
-    echo '</p>';
-
-
-    ?>
-
-    <table>
-        <tr><th>Real Name:</th><td>.$user['real_name'].</td></tr>
-        <tr><th>Email Address:</th><td>.$user['real_name'].</td></tr>
-        <tr><th>Year of Birth:</th><td>.$user['real_name'].</td></tr>
-        <tr><th>Access Level:</th><td>.$user['real_name'].</td></tr>
-    </table>
+    
+    echo '<table>';
+        echo '<tr><th>Real Name:</th><td>'.$user['real_name'].'</td></tr>';
+        echo '<tr><th>Email Address:</th><td>'.$user['email'].'</td></tr>';
+        echo '<tr><th>Year of Birth:</th><td>'.$user['dob'].'</td></tr>'; //TODO: Show Year
+        echo '<tr><th>Access Level:</th><td>'.$user['real_name'].'</td></tr>';
+        echo '</table>';
+        ?>
 </body>
 
 </html>

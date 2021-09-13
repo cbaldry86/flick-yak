5c771fa3f11bbce1518a3fa92797ef97cf896c29
Craig baldry <cbaldry@our.ecu.edu.au>
2021-09-13 09:21:38 +0800

    update path for search input

--- views/forms/search_form.php
+++ views/forms/search_form.php
@@ -1,4 +1,5 @@
 <?php
+//todo: add in validation
 echo '<form name="search" action="' . $views_dir . 'public/search_results_list.php' . '" method="get" onsubmit="">';
 echo '<div class="search-bar"><input type="submit" value="Search"><input type="text" name="search_movies"></div>';
 echo '</form>';

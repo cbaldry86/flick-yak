6aa4e1d0f58959d26944a87146e90a51151018e0
Craig baldry <cbaldry@our.ecu.edu.au>
2021-09-17 14:33:17 +0800

    add message if movie doesnt exist

--- views/public/movie_details.php
+++ views/public/movie_details.php
@@ -1,14 +1,14 @@
 <?php
-session_start();
 require '../../src/connect_db.php';
 $id;
-$is_post_Message_empty = false;
 if (!isset($_GET['id']) || !ctype_digit($_GET['id'])) {
-    echo 'Invalid movie ID.';
+    require '../errors/404.php';
     exit;
-}else {
+} else {
     $id = $_GET['id'];
 }
+
+session_start();
 
 if (isset($_POST['rate_submit'])) {
     //check if user has rating
@@ -50,9 +50,6 @@ if (isset($id)) {
     $stmt = $db->prepare("SELECT * FROM discussion WHERE movie_id = ? ORDER BY post_date DESC");
     $stmt->execute([$id]);
     $discussion = $stmt->fetchAll();
-} else {
-    echo 'Invalid movie ID.';
-    exit;
 }
 
 $title = 'Movie Details';
@@ -61,35 +58,38 @@ $css = '../../css/main.css';
 require_once '../common/head.php';
 require_once '../common/nav.php';
 
-echo '<div class="rating-container">';
-echo '<h1>' . $movie['movie_name'] . ' (' . $movie['release_year'] . ')</h1>';
-echo '<table></tbody>';
-echo '<tr><th>Director:</th><td>' . $movie['director'] . '</td></tr>';
-echo '<tr><th>Writers:</th><td>' . $movie['writers'] . '</td></tr>';
-echo '<tr><th>Duration:</th><td>' . $movie['duration'] . ' minutes</td></tr>';
-echo '<tr><th>Plot Summary:</th><td>' . $movie['summary'] . '</td></tr></tbody></table>';
-echo '<h2>Member Rating</h2><p>Average Rating: <b>' . number_format($rating['average_rating'], 1, '.', ',') . '</b></p>';
-
-if (isset($_SESSION['username'])) {
-    require_once '../forms/rating_form.php';
-}
-echo "<table><tbody>";
-foreach ($discussion as $row) {
-    echo '<tr><td><b>' . date("d/m/Y g:ia", strtotime($row['post_date']));
+if ($movie > 0) {
+    echo '<div class="rating-container">';
+    echo '<h1>' . $movie['movie_name'] . ' (' . $movie['release_year'] . ')</h1>';
+    echo '<table></tbody>';
+    echo '<tr><th>Director:</th><td>' . $movie['director'] . '</td></tr>';
+    echo '<tr><th>Writers:</th><td>' . $movie['writers'] . '</td></tr>';
+    echo '<tr><th>Duration:</th><td>' . $movie['duration'] . ' minutes</td></tr>';
+    echo '<tr><th>Plot Summary:</th><td>' . $movie['summary'] . '</td></tr></tbody></table>';
+    echo '<h2>Member Rating</h2><p>Average Rating: <b>' . number_format($rating['average_rating'], 1, '.', ',') . '</b></p>';
 
     if (isset($_SESSION['username'])) {
-        echo ' <a href="../member/user_profile.php?id=' . $row['username'] . '">('
-            . $row['username'] . ')</a>';
-    } else {
-        echo ' ' . $row['username'] . ' </b>';
+        require_once '../forms/rating_form.php';
     }
+    echo "<table><tbody>";
+    foreach ($discussion as $row) {
+        echo '<tr><td><b>' . date("d/m/Y g:ia", strtotime($row['post_date']));
 
-    echo '<p>' . $row['content'] . '</p></td></tr>';
+        if (isset($_SESSION['username'])) {
+            echo ' <a href="../member/user_profile.php?id=' . $row['username'] . '">('
+                . $row['username'] . ')</a>';
+        } else {
+            echo ' ' . $row['username'] . ' </b>';
+        }
+
+        echo '<p>' . $row['content'] . '</p></td></tr>';
+    }
+    echo "</tbody></table>";
+
+    if (isset($_SESSION['username'])) {
+        require_once '../forms/discussion_form.php';
+    }
+} else {
+    echo 'Movie does not exist please try another movie';
 }
-echo "</tbody></table>";
-
-if (isset($_SESSION['username'])) {
-    require_once '../forms/discussion_form.php';
-}
-
 echo '</div></div></body></html>';

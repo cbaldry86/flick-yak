49e71f91f386eff46da14c31c418844d49e831a8
Craig baldry <cbaldry@our.ecu.edu.au>
2021-10-26 15:16:05 +0800

    update login validation using hash

--- views/common/login.php
+++ views/common/login.php
@@ -2,11 +2,18 @@
 if (isset($_POST['login_submit'])) {
     require '../../src/connect_db.php';
 
-    $stmt = $db->prepare("SELECT username, access_level FROM user WHERE username = ? AND password = ?");
-    $success = $stmt->execute([$_POST['username_login'], $_POST['pass_login']]);
+    $stmt = $db->prepare("SELECT username, access_level, password FROM user WHERE username = ?");
+    $success = $stmt->execute([$_POST['username_login']]);
     $user = $stmt->fetch();
 
-    if (!$user) {
+    if ($user && password_verify($_POST['pass_login'], $user['password'])) {
+        session_start();
+        // Set session variables
+        $_SESSION["username"] = $user['username'];
+        $_SESSION["access_level"] = $user['access_level'];
+        header('Location: ../../index.php');
+
+    } else {
         $title = 'Login';
         $script = '../../scripts/main.js';
         $css = '../../css/main.css';
@@ -15,12 +22,6 @@ if (isset($_POST['login_submit'])) {
         echo '<h3>Failed To Sign In</h3>';
         echo '<p>Username or password was wrong please try again </p>';
         echo '</div></body></html>';
-    } else {
-        session_start();
-        // Set session variables
-        $_SESSION["username"] = $user['username'];
-        $_SESSION["access_level"] = $user['access_level'];
-        header('Location: ../../index.php');
     }
 } else {
     header('Location: ../../index.php');
